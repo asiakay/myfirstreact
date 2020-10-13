2ca1a89ecc012a7a284c388b9a9f6784a7769c8c
Asia Grady <66960776+asiakay@users.noreply.github.com>
2020-10-13 07:49:30 -0400

    props demo

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ class Car extends React.Component {
     this.state = {color: "red"};
   }
   render() {
-    return <h2>Hi, I am a {this.state.color} car!</h2>;
+    return <h2>Hi, I am a {this.props.color} car!</h2>;
   }
 }
 
-ReactDOM.render(<Car />, document.getElementById('root'));
+ReactDOM.render(<Car color="red"/>, document.getElementById('root'));
 
 
 
