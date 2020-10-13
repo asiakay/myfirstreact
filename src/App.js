c01d10ff6197cea6ea071764c00c8e299b1f02d7
Asia Grady <66960776+asiakay@users.noreply.github.com>
2020-10-13 12:49:33 -0400

    passing props through the constructor function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-class Car extends React.Component {
-    render(){
-        return <h2>Hi, I am a new car!</h2>;
-    }
-}
-export default Car;
+// class Car extends React.Component {
+//     render(){
+//         return <h2>Hi, I am a new car!</h2>;
+//     }
+// }
+// export default Car;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Car from './App.js';
 
-ReactDOM.render(<Car />, document.getElementById('root'));
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h2>I am a {this.props.model}!</h2>;
+  }
+}
 
 
-
+ReactDOM.render(<Car model="Mustang" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
