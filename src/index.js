f980bc4d8c9c2009a559df60c48219c95db521cf
Asia Grady <66960776+asiakay@users.noreply.github.com>
2020-10-13 07:43:25 -0400

    Component constructor example

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Car extends React.Component {
+  constructor() {
+    super();
+    this.state = {color: "red"};
+  }
   render() {
-    return <h2>Hi, I am a car!</h2>;
+    return <h2>Hi, I am a {this.state.color} car!</h2>;
   }
 }
 
